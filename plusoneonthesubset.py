9c1652cd465d8559ab1442deba294d7528537923
Cai0n29 <capellakratos@gmail.com>
2023-07-12 10:33:20 +0800

    (Plus one on the subset) problem solution

---
+++ plusoneonthesubset.py
@@ -0,0 +1,17 @@
+p = int(input())
+for i in range(p):
+    n = int(input())
+    l = input().split()
+    ini = [int(x) for x in l]
+    maxi = 0
+    mini = 0
+    maxi = max(ini)
+    mini = min(ini)
+    print(maxi - mini)
+
+
+
+    
+
+
+
